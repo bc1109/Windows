2c283d3455a404fa0547727b8f3ee5ad3ba9f119
Brandon Christopher <Brandon.Christopher@gmail.com>
2020-05-29 14:38:32 -0400

    updates

--- TL1.py
+++ TL1.py
@@ -4,29 +4,29 @@ import os
 import sys
 
 
+# Begin TACLAN YAML
 if os.path.isfile('Additional_VLANs.yml'):
     vlanfile = open("Additional_VLANs.yml")
     parsed_vlanfile = yaml.load(vlanfile, Loader=yaml.FullLoader)
-    os.close()
-    # need to see if we have to close this - could be the dump but looks like an additional way to parse the yaml file further.
-    # need to find out how to close this file since we now have it within the parsed_vlanfile variable.
+    vlanfile.close()
 else:
-    # if this is true I want to stop the script bc the script will error out.
-    #print('There is an issue with locating the Additional_VLANs.yml file!')
     sys.exit('There is an issue with locating the Additional_VLANS.yml file!')
 
-# Define YAML Values
+# Begin define YAML Values
 # for loop would probably be ideal here. this will work for now.
+
+for value in parsed_vlanfile:
+
+
 FirstVLANname = (parsed_vlanfile.get('V1name'))  # - this worked.
 
-# DTECH JSON Begin
+# Begin DTECH JSON
 if os.path.isfile('C:\\TACLAN\\Config\\JSONs\\dtconfig.init.json'):
     #print('Parsing the dtconfig json file')
     json_file = open("dtconfig.init.json")
     dtconfig = json.load(json_file)
-    # json_file.close()
+    json_file.close()
 else:
-    #print('There is an issue with locating the dtconfig.json file!')
     sys.exit('There is an issue with locating the dtconfig.json file!')
 
 switchusername = (dtconfig['setup']['Network']['SwitchUser'])
--- TL1_os.py
+++ TL1_os.py
@@ -7,4 +7,4 @@ import sys
 if os.path.isfile('Additional_VLANs.yml'):
     vlanfile = open("Additional_VLANs.yml")
     parsed_vlanfile = yaml.load(vlanfile, Loader=yaml.FullLoader)
-    # vlanfile.close
+    vlanfile.close()
